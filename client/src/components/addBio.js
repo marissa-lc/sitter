11a857a6bbf27235e540ad63525f17217406949a
RIssa <rissa_cford@yahoo.com>
2020-04-09 12:53:01 -0700

    addBio.js

--- client/src/components/addBio.js
+++ client/src/components/addBio.js
@@ -1,57 +1,56 @@
-import React, { Component } from "react";
-import {Textfield, Grid, Cell, FABButton, Icon } from 'react-mdl';
+import React from "react";
+import { Button, Modal } from 'react-bootstrap';
 
-class AddBio extends Component {
-    render() {
-        return(
-    <div>
-        <Grid className="profileForm">
-          <Cell col={6}>
-            <h2>
-                Add A Profile
-            </h2>
+function AddBio(props) {
+  console.log("event props", props);
 
-            <hr/>
+  return (
+    <Modal
+    {...props}
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered>
+            <div class="card text-center">
+                  <div class="card-header">
+                    Add a Profile
+                  </div>
+          
+                  <form>
+                      <div class="form-row">
+                        <div class="form-group col-md-6">
+                          <label for="name">Name</label>
+                          <input type="name" class="form-control" id="name" placeholder="Name"/>
+                        </div>
+                       
+                        <div class="form-group col-md-6">
+                          <label for="age">Age</label>
+                          <input type="age" class="form-control" id="age" placeholder="Age"/>
+                        </div>
+          
+                        <div class="form-group col-md-6">
+                          <label for="input">Who is this profile for?</label>
+                          <select id="input" class="form-control">
+                                  <option>Child</option>
+                                  <option>Pet</option>
+                          </select>
+                          </div>
+          
+                        <div class="form-group">
+                          <label for="bio">Tell us a little more...</label>
+                          <textarea class="form-control" id="bio" rows="3"></textarea>
+                        </div>
+                        
+                        </div>
+                      {/* <button type="submit" class="btn btn-primary">Submit</button> */}
+                  </form>
+              </div>
+   
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
 
-            <div className="form">
-            <Textfield
-            onChange={() => {}}
-            label="Name..."
-            style={{width: '200px'}}>
-             </Textfield>
-             </div>
-    
-            <div>
-            <Textfield
-            onChange={() => {}}
-            pattern="-?[0-9]*(\.[0-9]+)?"
-            error="Input is not a number!"
-            label="Age..."
-            style={{width: '200px'}}>
-            </Textfield>
-            </div>
-
-            <div>
-            <Textfield
-            onChange={() => {}}
-            label="Tell us more..."
-            style={{width: '600px'}}>
-             </Textfield>
-             </div>
-
-            <div>
-             <FABButton colored ripple>
-            <Icon name="add" />
-            </FABButton>
-            </div>
-
-          </Cell>
-        </Grid>
-      </div>        
-
- 
-        )
-    }
+    </Modal>
+  );
 }
 
 export default AddBio;
