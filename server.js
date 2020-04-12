d4dff735e5c305940ae2c78f449b3f0eba9e2465
Sydney <sguitjens@gmail.com>
2020-04-12 15:35:51 -0700

    more heroku tweeks

--- server.js
+++ server.js
@@ -32,11 +32,10 @@ app.use(cookieParser());
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // React client routes
 // require("./routes/index.js")(app)
