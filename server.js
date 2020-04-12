1b2afe6510f9d6db054e4799c68e00cb3a309117
Sydney <sguitjens@gmail.com>
2020-04-12 15:28:41 -0700

    add some stuff for Heroku

--- server.js
+++ server.js
@@ -32,6 +32,12 @@ app.use(cookieParser());
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // React client routes
 // require("./routes/index.js")(app)
 
